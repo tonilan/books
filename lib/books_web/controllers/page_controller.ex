ef41288746ab5966db6e76ca9c56cab221482a84
ifsc01 <ifsclimbing@gmail.com>
2018-04-03 12:33:06 +0800

    chore: update server name

--- lib/books_web/controllers/page_controller.ex
+++ lib/books_web/controllers/page_controller.ex
@@ -6,12 +6,14 @@ defmodule BooksWeb.PageController do
     render conn, "index.html"
   end
 
+  # https://books.dsh.li/surge?password=aaa&username=96481
   def surge(conn, %{"username" => username, "password" => password}) do
     url = current_url(conn)
     s5_servers = Servers.socks5_tls()
     render conn, "surge.text", url: url, username: username, password: password, s5_servers: s5_servers
   end
 
+  # https://books.dsh.li/ssr?password=aaa&port=96481
   def ssr(conn, %{"port" => port, "password" => password}) do
     text conn, Servers.ssr_text(port, password)
   end
