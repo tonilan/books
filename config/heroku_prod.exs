9dc7f7cbb53742fab72bd9f7b2c52196a468f315
lds <ldshuang@gmail.com>
2018-08-31 17:54:12 +0800

    update config/heroku_prod.exs

--- config/heroku_prod.exs
+++ config/heroku_prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :books, BooksWeb.Endpoint,
   load_from_system_env: true,
-  http: [port: 80],
+  url: [scheme: "https", host: "books-sub-api.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
