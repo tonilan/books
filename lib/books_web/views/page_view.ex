5992f812dbcddcef14d58bf843a6630d3a9d83b4
lds <ldshuang@gmail.com>
2018-10-14 00:39:02 +0800

    fix: reomve ss name

--- lib/books_web/views/page_view.ex
+++ lib/books_web/views/page_view.ex
@@ -2,6 +2,6 @@ defmodule BooksWeb.PageView do
   use BooksWeb, :view
 
   def ss_nodes do
-    Enum.map_join(Books.Servers.ss_server(), ", ", fn s -> "#{s.name} SS" end)
+    Enum.map_join(Books.Servers.ss_server(), ", ", fn s -> s.name end)
   end
 end
